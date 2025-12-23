c7f62dfb691514320a3b97b32c14c62b13207796
PeterHW963 <peterhadiwijaya963@gmail.com>
2025-12-23 08:37:09 +0800

    fix formatting

--- frontend/src/components/admin/ProductManagement.tsx
+++ frontend/src/components/admin/ProductManagement.tsx
@@ -2,109 +2,116 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { useEffect } from "react";
 import {
-	deleteProduct,
-	fetchAdminProducts,
-	fetchProductVariants,
+  deleteProduct,
+  fetchAdminProducts,
+  fetchProductVariants,
 } from "../../redux/slices/adminProductSlice";
 import { cloudinaryImageUrl } from "../../constants/cloudinary";
 
 const ProductManagement = () => {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
-	const { user } = useAppSelector((state) => state.auth);
-	const { products, productVariants, loading, error } = useAppSelector(
-		(state) => state.adminProducts
-	);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { user } = useAppSelector((state) => state.auth);
+  const { products, productVariants, loading, error } = useAppSelector(
+    (state) => state.adminProducts
+  );
 
-	useEffect(() => {
-		if (!user || user.role !== "admin") {
-			navigate("/");
-			return;
-		} 
-		if (products.length === 0 && !loading) {
-			dispatch(fetchAdminProducts());
-		}
-		const hasLoadedVariants = Object.keys(productVariants).length > 0;
+  useEffect(() => {
+    if (!user || user.role !== "admin") {
+      navigate("/");
+      return;
+    }
+    if (products.length === 0 && !loading) {
+      dispatch(fetchAdminProducts());
+    }
+    const hasLoadedVariants = Object.keys(productVariants).length > 0;
 
-		if (products.length > 0 && !loading && !hasLoadedVariants) {
-			const productIds = products.map((p) => p._id);
-			dispatch(fetchProductVariants({ productIds }));
-		}
+    if (products.length > 0 && !loading && !hasLoadedVariants) {
+      const productIds = products.map((p) => p._id);
+      dispatch(fetchProductVariants({ productIds }));
+    }
+  }, [dispatch, user, navigate, products, loading, productVariants, error]);
 
-	}, [dispatch, user, navigate, products, loading, productVariants, error]);
+  const handleDelete = (productId: string) => {
+    if (window.confirm("Are you sure you want to delete the Product?")) {
+      dispatch(deleteProduct(productId));
+    }
+  };
 
-	const handleDelete = (productId: string) => {
-		if (window.confirm("Are you sure you want to delete the Product?")) {
-			dispatch(deleteProduct(productId));
-		}
-	};
-
-	// prevent double "Loading..."
-	const isFullyLoaded = products.length > 0 && Object.keys(productVariants).length > 0;
-	const showLoading = loading || !isFullyLoaded;
-	if (showLoading) return <p>Loading...</p>;
-	if (error) return <p>Error: {error}</p>;
-	return (
-		<div className="max-w-7xl mx-auto p-6">
-			<h2 className="text-2xl font-bold mb-4">Product Management</h2>
-			<div className="overflow-x-auto shadow-md sm:rounded-lg">
-				<table className="min-w-full text-left text-gray-500">
-					<thead className="bg-gray-100 text-xs uppercase text-gray-700">
-						<tr>
-							<th className="py-3 px-4">Image</th>
-							<th className="py-3 px-4">Name</th>
-							<th className="py-3 px-4">Price (IDR)</th>
-							<th className="py-3 px-4">Actions</th>
-						</tr>
-					</thead>
-					<tbody>
-						{products.length > 0 ? (
-							products.map((product) => {
-								const allVariants = productVariants[product._id] || [];
-            const defaultVariant = allVariants.find(v => v.isDefault) || allVariants[0];
-			const displayPrice = defaultVariant?.discountPrice ?? defaultVariant?.price
-								return (
-								
-								<tr
-									key={product._id}
-									className="border-b hover:bg-gray-50 cursor-pointer"
-								>
-									<td className="p-4"><img 
-                    src={cloudinaryImageUrl(defaultVariant?.images?.[0]?.publicId)}  alt={product.name} 
-                    className="w-12 h-12 object-cover" 
-                /></td>
-									<td className="p-4 font-medium text-gray-900 whitespace-nowrap">
-										{product.name}
-									</td>
-									<td className="p-4">{displayPrice.toLocaleString()}</td>
-									<td className="p-4">
-										<Link
-											to={`/admin/products/${product._id}/edit/${defaultVariant._id}`}
-											className="bg-yellow-500 text-white px-2 py-1 rounded mr-2 hover:bg-yellow-600"
-										>
-											Edit
-										</Link>
-										<button
-											onClick={() => handleDelete(product._id)}
-											className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
-										>
-											Delete
-										</button>
-									</td>
-								</tr>
-							)})
-						) : (
-							<tr>
-								<td colSpan={4} className="p-4 text-center text-gray-500">
-									No products found.
-								</td>
-							</tr>
-						)}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+  // prevent double "Loading..."
+  const isFullyLoaded =
+    products.length > 0 && Object.keys(productVariants).length > 0;
+  const showLoading = loading || !isFullyLoaded;
+  if (showLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h2 className="text-2xl font-bold mb-4">Product Management</h2>
+      <div className="overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="min-w-full text-left text-gray-500">
+          <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+            <tr>
+              <th className="py-3 px-4">Image</th>
+              <th className="py-3 px-4">Name</th>
+              <th className="py-3 px-4">Price (IDR)</th>
+              <th className="py-3 px-4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.length > 0 ? (
+              products.map((product) => {
+                const allVariants = productVariants[product._id] || [];
+                const defaultVariant =
+                  allVariants.find((v) => v.isDefault) || allVariants[0];
+                const displayPrice =
+                  defaultVariant?.discountPrice ?? defaultVariant?.price;
+                return (
+                  <tr
+                    key={product._id}
+                    className="border-b hover:bg-gray-50 cursor-pointer"
+                  >
+                    <td className="p-4">
+                      <img
+                        src={cloudinaryImageUrl(
+                          defaultVariant?.images?.[0]?.publicId
+                        )}
+                        alt={product.name}
+                        className="w-12 h-12 object-cover"
+                      />
+                    </td>
+                    <td className="p-4 font-medium text-gray-900 whitespace-nowrap">
+                      {product.name}
+                    </td>
+                    <td className="p-4">{displayPrice.toLocaleString()}</td>
+                    <td className="p-4">
+                      <Link
+                        to={`/admin/products/${product._id}/edit/${defaultVariant._id}`}
+                        className="bg-yellow-500 text-white px-2 py-1 rounded mr-2 hover:bg-yellow-600"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        onClick={() => handleDelete(product._id)}
+                        className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan={4} className="p-4 text-center text-gray-500">
+                  No products found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ProductManagement;
