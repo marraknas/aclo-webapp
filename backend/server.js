c5e77c72049d2fef9ce9fc1c8a20ff8588cfa192
PeterHW963 <peterhadiwijaya963@gmail.com>
2025-12-31 14:45:02 +0800

    fix seed data

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,7 @@ app.use("/api/upload", uploadRoutes); // prepends /api/upload to all the upload 
 app.use("/api/subscribe", subscribeRoutes); // prepends /api/subscribe to all the subscribe routes
 app.use("/api/reviews", reviewRoutes); //prepends /api/reviews to all the subscribe routes
 
+// NOT USED FOR NOW
 app.use("/api/payments/midtrans", midtransRoutes); // prepends /api/payments/midtrans for midtrans payment route
 app.use("/api/webhooks/midtrans", midtransWebhook);
 
